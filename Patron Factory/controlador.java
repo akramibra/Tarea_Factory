7d4c20ab9b30e901dafece45d094893d3c1a6149
akramibra <ibr211645@uvg.edu.gt>
2023-02-10 12:09:56 -0600

    Entrega final

--- Patron Factory/controlador.java
+++ Patron Factory/controlador.java
@@ -17,18 +17,21 @@ dispositivos electronics = new dispositivos();
             electronics.laptop();
 
         }else if(opcion ==3){
+            electronics.ipad();
 
         }else if (opcion ==4){
+            electronics.appleWatch();
 
         }else if (opcion ==5){
+            electronics.appleTv();
 
         }else if (opcion ==6){
+            electronics.ipod();
 
         }else if (opcion ==7){
+            electronics.iphone3();
 
         }else if (opcion ==8){
-
-        }else if (opcion ==9 ){
 
         }else{
             System.out.println("Escoga una opcion valida");
--- Patron Factory/dispositivos.java
+++ Patron Factory/dispositivos.java
@@ -15,18 +15,18 @@ public class dispositivos{
     }
 
     public void appleWatch(){
-
+        System.out.println("Un reloj normal ?? pruebe ese appleWatch que podra conectar a su telefono");
     }
 
     public void appleTv(){
-
+        System.out.println("Hoy es noche de peliculas, disfrute el Apple TV con 4k de resolucion, busque la pelicula que mas le gusta");
     }
 
     public void ipod(){
-
+        System.out.println("Le toco un ipod unico, tiene 4GB de ram, guarde esa dispositivo tan clasico");
     }
 
     public void iphone3(){
-
+        System.out.println("Que suerte la suya, le toco un telefono muy dificil de encontrar, tiene 16 GB de ram y es de color azul");
     }
 }
